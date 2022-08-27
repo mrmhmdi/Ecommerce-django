c57288597f2b7fde2680e29176d47157a2bf0c8c
mohammadreza <mr.mhdi1995@gmail.com>
2022-08-28 00:57:30 +0430

    debug context_processor and rewrite user

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,7 +1,11 @@
 from .models import Cart
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, HttpResponse
 
 
 def cart_globally(request):
-    cart = get_object_or_404(Cart, user=request.user, completed=False)
-    return {'cart_g': cart.cartitem.all()}
+    if request.user.is_authenticated:
+        cart, created_cart = Cart.objects.get_or_create(
+            user=request.user, completed=False)
+        return {'cart_g': cart.cartitem.all()}
+    else:
+        return {'cart_g': {}}
--- core/models.py
+++ core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
-# class CustomeUser(AbstractUser):
-#     pass
+class CustomeUser(AbstractUser):
+    pass

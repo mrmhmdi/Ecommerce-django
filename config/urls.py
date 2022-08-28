db3348c2a62991fd8ad4819d6e8445cdf07d894b
mohammadreza <mr.mhdi1995@gmail.com>
2022-08-28 07:59:32 +0430

    added image to models and define get_thumbnail

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('add_to_cart/', include('carts.urls'))
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
+from django.core.files import File
+
+from PIL import Image
+from io import BytesIO
 # Create your models here.
 
 
@@ -31,6 +35,8 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/', blank=True)
+    thumbnail = models.ImageField(upload_to='product_thumbnails/', blank=True)
 
     class Meta:
         ordering = ('-created_at',)
@@ -42,6 +48,29 @@ class Product(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            print(self.thumbnail.url)
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self._create_thumbnail(self.image)
+                self.save()
+                return self.thumbnail.url
+            else:
+                return 'https://via.placeholder.com/240x240x.jpg'
+
+    def _create_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+        return thumbnail
 
     def __str__(self) -> str:
         return self.name
